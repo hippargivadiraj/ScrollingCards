dff70a281d1c341363eb81b93d88da1f61fd9355
Vadiraj Hippargi <Vadi@leadconsultant.com>
2023-10-17 19:31:36 -0700

    Initial Commit

---
+++ ScrollingCards/ScrollingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollingCardsApp.swift
+//  ScrollingCards
+//
+//  Created by Vadiraj Hippargi on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrollingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
